9a784a19ce4316c01dbe4fd61e8e65193abb1c33
Rafael Escrich <rafaelescrich@gmail.com>
2016-07-20 19:33:38 -0300

    First commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/aronrodrigues/pen/YXvBWW.
+
+ https://thinkster.io/angularjs-jwt-auth/
---
+++ js/index.js
@@ -0,0 +1,126 @@
+;(function(){
+function authInterceptor(API, auth) {
+  return {
+    // automatically attach Authorization header
+    request: function(config) {
+      var token = auth.getToken();
+      if(config.url.indexOf(API) === 0 && token) {
+        config.headers.Authorization = 'Bearer ' + token;
+      }
+      return config;
+    },
+
+    response: function(res) {
+      if(res.config.url.indexOf(API) === 0 && res.data.token) {
+        auth.saveToken(res.data.token);
+      }
+
+      return res;
+    },
+  }
+}
+
+function authService($window) {
+  var srvc = this;
+
+  srvc.parseJwt = function (token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace('-', '+').replace('_', '/');
+    return JSON.parse($window.atob(base64));
+  };
+
+  srvc.saveToken = function (token) {
+    $window.localStorage['jwtToken'] = token
+  };
+
+  srvc.logout = function (token) {
+    $window.localStorage.removeItem('jwtToken');
+  };
+
+  srvc.getToken = function () {
+    return $window.localStorage['jwtToken'];
+  };
+
+  srvc.isAuthed = function () {
+    var token = srvc.getToken();
+    if (token) {
+      var params = srvc.parseJwt(token);
+      return Math.round(new Date().getTime() / 1000) <= params.exp;
+    } else {
+      return false;
+    }
+  }
+
+}
+
+function userService($http, API, auth) {
+  var srvc = this;
+  srvc.getQuote = function() {
+    return $http.get(API + '/auth/quote')
+  }
+  srvc.register = function (username, password) {
+    return $http.post(API + '/auth/register', {
+      email: username,
+      password: password
+    });
+  };
+
+  srvc.login = function (username, password) {
+    return $http.post(API + '/auth/sign_in', {
+      email: username,
+      password: password
+    });
+  };
+
+  srvc.myActivities = function() {
+    return $http.get(API + '/my_activities')
+  };
+
+}
+
+// We won't touch anything in here
+function MainCtrl(user, auth) {
+  var self = this;
+
+  function handleRequest(res) {
+    var token = res.data ? res.data.token : null;
+    if(token) {
+      console.log('JWT:', token);
+    }
+    self.message = res.data.message;
+  }
+
+  self.login = function() {
+    user.login(self.username, self.password)
+      .then(handleRequest, handleRequest)
+  }
+  self.register = function() {
+    user.register(self.username, self.password)
+      .then(handleRequest, handleRequest)
+  }
+  self.getQuote = function() {
+    user.getQuote()
+      .then(handleRequest, handleRequest)
+  }
+  self.logout = function() {
+    auth.logout && auth.logout()
+  }
+  self.isAuthed = function() {
+    return auth.isAuthed ? auth.isAuthed() : false
+  }
+  self.myActivities = function() {
+    user.myActivities()
+      .then(handleRequest, handleRequest)
+  }
+}
+
+angular.module('app', [])
+.factory('authInterceptor', authInterceptor)
+.service('user', userService)
+.service('auth', authService)
+.constant('API', 'http://portalmecapi.c3sl.ufpr.br/v1')
+.config(function($httpProvider) {
+  $httpProvider.interceptors.push('authInterceptor');
+})
+.controller('Main', MainCtrl)
+})();
